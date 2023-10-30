f23fcb7e7a4677677ab553953f6fb367175b01f4
ThatCreep2089 <76827300+ThatCreep2089@users.noreply.github.com>
2023-10-30 14:13:19 +0100

    Create Program.cs
    joe momma

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace pruebaGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("joe momma");
+
+        }
+    }
+}
